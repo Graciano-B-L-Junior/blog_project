db2ca45b57cb421e136d4f21c94fbebeaa2447fd
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2026-09-07 17:52:16 -0300

    Add Comment model, forms, and admin integration

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from .models import Post
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 # Create your views here.
 
 def post_list(request):
@@ -89,5 +89,32 @@ def post_share(request, post_id):
             'post':post,
             'form':form,
             'sent':sent
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
         }
     )
