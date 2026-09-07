e2c22cc9ed91750f067e298f4bbe6e52686449b8
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2026-09-07 18:17:25 -0300

    stopped at page 102

--- blog/views.py
+++ blog/views.py
@@ -35,10 +35,18 @@ def post_detail(request, year, month, day, post):
         publish__day=day
     )
 
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {
+            'post': post,
+            'comments':comments,
+            'form':form
+        }
     )
 
 class PostListView(ListView):
