e7f81ffb372c99b69b9b575f6ab574e8329b5ef3
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2026-09-10 17:50:43 -0300

    Enhance post search functionality with ranking and improved query handling

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector, SearchQuery, SearchRank
+)
 
 from taggit.models import Tag
 
@@ -22,10 +24,14 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body')
+                    search=search_vector,
+                    rank=SearchRank(search_vector,search_query)
                 ).filter(search=query)
+                .order_by('-rank')
             )
 
     return render(
